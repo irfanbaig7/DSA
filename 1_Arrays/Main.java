1fb1e4f9180f41920517ccd9e23a583eec821d0e
irfanbaig7 <ibaig9356@gmail.com>
2025-01-18 23:01:43 +0530

    Main File Added

---
+++ 1_Arrays/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Store a number
+        // int rno1 = 19;
+        // int rno2 = 20;
+        // int rno3 = 21;
+        // int rno4 = 22;
+        // System.out.println(rno1);
+        // System.out.println(rno2);
+        // System.out.println(rno3);
+        // System.out.println(rno4);
+
+        // int[] studentRollNo = { 19, 20, 21, 22 };
+        // studentRollNo[0] = 18;
+        // for (int i : studentRollNo) {
+        //     System.out.println(i);
+        // }
+
+        // int[] rollNum = new int[5];
+        // rollNum[0] = 20;
+        // rollNum[1] = 21;
+        // rollNum[2] = 22;
+        // rollNum[3] = 23;
+        // rollNum[4] = 24;
+        // for (int i : rollNum) {
+        //     System.out.println(i);
+        // }
+        // System.out.println("Rollno1 : " + rollNum[0]);
+
+
+
+        // String Array
+        // String[] studentNames = {"irfan", "karan", "rohit", "sumit"};
+        // for (String string : studentNames) {
+        //     System.out.println(string);
+        // }
+
+        // String[] palyersName = new String[3];
+        // palyersName[0] = "Vijay";
+        // palyersName[1] = "dhoni";
+        // palyersName[2] = "Virat";
+        // for (String string : palyersName) {
+        //     System.out.println(string);
+        // }
+
+        
+
+    }
+}
