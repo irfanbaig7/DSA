17d089b411f51b220f63bd77ef648561a1514344
irfanbaig7 <ibaig9356@gmail.com>
2025-01-23 15:48:09 +0530

    input file added

--- 1_Arrays/Input.java
+++ 1_Arrays/Input.java
@@ -1,29 +1,125 @@
 
-import java.util.Arrays;
 import java.util.Scanner;
 public class Input {
     
     public static void main(String[] args) {
         
         // Input using Loop
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Fill the Seats num : ");
-        int[] arr = new int[5];
-        for (int i = 0; i < arr.length; i++) {
-           arr[i] = sc.nextInt();
-        }
-        System.out.println(Arrays.toString(arr));
 
+        // ***** Array Of Primatives
+
+        Scanner sc = new Scanner(System.in);
+
+        // int[] arr = new int[5];
+        // // System.out.println(arr[0]);    //BY Default value was 0
+        // arr[0] = 10;
+        // arr[1] = 20;
+        // arr[2] = 30;
+        // arr[3] = 40;
+        // arr[4] = 50;
+        // System.out.println(arr[4]);
+
+
+        // taking input printing output (integers)
+
+        // int[] arr = new int[5];
+        // // System.out.println(arr[0]);    //BY Default value was 0
+        // System.out.print("Enter YOur 1 num : ");
+        // arr[0] = sc.nextInt();
+        // System.out.print("Enter YOur 2 num : ");
+        // arr[1] = sc.nextInt();
+        // System.out.print("Enter YOur 3 num : ");
+        // arr[2] = sc.nextInt();
+        // System.out.print("Enter YOur 4 num : ");
+        // arr[3] = sc.nextInt();
+        // System.out.print("Enter YOur 5 num : ");
+        // arr[4] = sc.nextInt();
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i]);
+        // }
+
+
+        // Taking input printing output (String)
+
+        // System.out.println("Enter Your arrays length : ");
+        // int length = sc.nextInt();
+        // String[] arr = new String[length];
+
+        // String[] arr = new String[5];
+        // System.out.println(arr[0]);    //BY Default value was 0
+        // System.out.print("Enter YOur 1 word : ");
+        // arr[0] = sc.next();
+        // System.out.print("Enter YOur 2 word : ");
+        // arr[1] = sc.next();
+        // System.out.print("Enter YOur 3 word : ");
+        // arr[2] = sc.next();
+        // System.out.print("Enter YOur 4 word : ");
+        // arr[3] = sc.next();
+        // System.out.print("Enter YOur 5 word : ");
+        // arr[4] = sc.next();
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i]);
+        // }
+
+
+        
+
+
+
+
+
+
+
+
+
+        // input using for loop
+        // int[] arr = new int[5];
+
+        // step 1
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
 
         // for (int i = 0; i < arr.length; i++) {
-        //    arr[i] = sc.nextInt();
-        // }
-        
-        // for (int i : arr) {
-        //     System.out.print(i + " ");
+        //     System.out.println(arr[i]);
         // }
 
+        // Step 2
+        // for (int i : arr) {
+        //     i = sc.nextInt();
+        //     System.out.println(i);
+        // }
+
+        // step 3
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
+        // System.out.println(Arrays.toString(arr));
+    
+
+
+        // ***** Array Of Object's
+        // String[] str = new String[5];
+    
+        // for (int i = 0; i < str.length; i++) {
+        //     str[i] = sc.next();
+        // }
+        // System.out.println(Arrays.toString(str));
+
+
+
+        // Modify
+        // str[1] = "hkdjhk";
+        // System.out.println(Arrays.toString(str));
+
+
+
         
+
+
+
+
+
 
 
         sc.close();
--- 1_Arrays/Main.java
+++ 1_Arrays/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Store a number
+
         // int rno1 = 19;
         // int rno2 = 20;
         // int rno3 = 21;
@@ -12,11 +13,17 @@ public class Main {
         // System.out.println(rno3);
         // System.out.println(rno4);
 
+
+
+
+        // integeres Arrays
+
         // int[] studentRollNo = { 19, 20, 21, 22 };
         // studentRollNo[0] = 18;
         // for (int i : studentRollNo) {
         //     System.out.println(i);
         // }
+
 
         // int[] rollNum = new int[5];
         // rollNum[0] = 20;
@@ -27,21 +34,22 @@ public class Main {
         // for (int i : rollNum) {
         //     System.out.println(i);
         // }
-        // System.out.println("Rollno1 : " + rollNum[0]);
-
 
 
         // String Array
+
         // String[] studentNames = {"irfan", "karan", "rohit", "sumit"};
         // for (String string : studentNames) {
         //     System.out.println(string);
         // }
 
-        // String[] palyersName = new String[3];
-        // palyersName[0] = "Vijay";
-        // palyersName[1] = "dhoni";
-        // palyersName[2] = "Virat";
-        // for (String string : palyersName) {
+        // String[] arr = new String[3];
+        // System.out.println(arr[0]); // Bydefualt value was NULL
+        
+        // arr[0] = "Vijay";
+        // arr[1] = "dhoni";
+        // arr[2] = "Virat";
+        // for (String string : arr) {     //For-Each Loop
         //     System.out.println(string);
         // }
 
