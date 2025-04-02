46f806d5db69aaf3de100b6a13581e5f75939e91
irfanbaig7 <ibaig9356@gmail.com>
2025-04-02 16:22:26 +0530

    diff way to FindAllNumDisappeared modify add

--- 7_CycleSort/FindAllNumDisappeared.java
+++ 7_CycleSort/FindAllNumDisappeared.java
@@ -6,7 +6,7 @@ public class FindAllNumDisappeared {
 
     public static void main(String[] args) {
         // int[] arr = {5, 2, 3, 1, 4 };
-        int[] arr = { 4,  3, 2, 7, 8, 2, 3, 1 }; // correct idx will be
+        int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 }; // correct idx will be
         // cycleSort(arr);
         // System.out.println(Arrays.toString(arr));
         System.out.println(cycleSort(arr));
@@ -16,8 +16,8 @@ public class FindAllNumDisappeared {
         int i = 0;
         while (i < arr.length) {
             int coorrectIndex = arr[i] - 1;
-            // if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[coorrectIndex]) {
-            if (arr[i] != arr[coorrectIndex]) {
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[coorrectIndex]) {
+            // if (arr[i] != arr[coorrectIndex]) {
                 swap(arr, i, coorrectIndex);
             } else {
                 i++;
