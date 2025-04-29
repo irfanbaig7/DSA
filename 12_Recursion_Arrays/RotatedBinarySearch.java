8d7427df6ffb371e632950dcedaccb5f5e9f89b5
irfanbaig7 <ibaig9356@gmail.com>
2025-04-29 08:42:47 +0530

    try this code with diffrent target valuse

--- 12_Recursion_Arrays/RotatedBinarySearch.java
+++ 12_Recursion_Arrays/RotatedBinarySearch.java
@@ -1,6 +1,6 @@
 public class RotatedBinarySearch {
     public static void main(String[] args) {
-        int [] arr = {5, 6, 7, 8, 1, 2, 3, 4};
+        int [] arr = {5, 6, 7, 8, 9, 1, 2, 3};
         System.out.println(search(arr, 6, 0, arr.length - 1));
     }
 
