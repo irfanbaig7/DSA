c9565d4df04995b4c85bdbc611675e86b4dad325
irfanbaig7 <ibaig9356@gmail.com>
2025-04-29 07:31:34 +0530

    add result using List

---
+++ 12_Recursion_Arrays/AddingOnListAnotherWay.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddingOnListAnotherWay {
+    public static void main(String[] args) {
+        int[] arr = { 3, 2, 18, 1, 18, 9, 18, 25, 36, 18 };
+        int target = 18;
+        int index = 0;
+        findAllIndex(arr, target, index);
+        System.out.println(List);
+    }
+
+    // static ArrayList<Integer> List = new ArrayList<>();
+    static ArrayList findAllIndex(int[] arr, int target, int index, ArrayList<Integer> List) {
+        if (index == arr.length) {
+            return List; 
+        }
+        if (arr[index] == target) {
+            List.add(index);
+        }
+        findAllIndex(arr, target, index + 1);
+
+    }
+}
